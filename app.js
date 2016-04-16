aa3c081fa73a4d1d7f720611331bdf336781ca85
Qiming Chen <qimche@fb.com>
2016-04-16 14:20:40 -0700

    mongoose init

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://treeify:treeify@ds011251.mlab.com:11251/treeify');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to mongoose!');
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });

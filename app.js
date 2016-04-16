1a09174e37f5fb65522778dc5fcd6bd37bf14234
Qiming Chen <qimche@fb.com>
2016-04-16 14:09:01 -0700

    static files, port 80

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Treeify app listening on port 80!');
 });
 
 

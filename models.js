3d50add07ce55ed0bd68772f4ec0e74ec81edf6a
Qiming Chen <qimche@fb.com>
2016-04-16 16:10:12 -0700

    i'm bad at git

---
+++ models.js
@@ -0,0 +1,91 @@
+//=========DB=========
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://treeify:treeify@ds011251.mlab.com:11251/treeify');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to mongoose!');
+});
+
+//=========SCHEMAS=========
+
+var sessionsSchema = mongoose.Schema({
+    _id: String,
+    u_id: String
+});
+
+var treesSchema = mongoose.Schema({
+    _id: String,
+    type: String,
+    location: {
+		x: Number,
+		y: Number
+    },
+    plant_time: Number
+});
+
+var typesSchema = mongoose.Schema({
+    _id: String,
+    name: String,
+    o2rate: Number,
+    growth_rate: Number,
+    life_span: Number
+});
+
+var usersSchema = mongoose.Schema({
+    _id: String,
+    name: String,
+    email: String,
+    type: Number,
+    trees: [String]
+});
+
+//=========MODELS=========
+
+var Sessions = mongoose.model('Sessions', sessionsSchema);
+var Trees = mongoose.model('Trees', treesSchema);
+var Types = mongoose.model('Types', typesSchema);
+var Users = mongoose.model('Users', usersSchema);
+
+//=========EXPORT=========
+
+exports.Sessions = Sessions;
+exports.Trees = Trees;
+exports.Types = Types;
+exports.Users = Users;
+
+//=========LOGGING=========
+
+Sessions.find(function (err, sessions) {
+    if (err) 
+        return console.error(err);
+    console.log();
+    console.log("Sessions:");
+    console.log(sessions);
+})
+
+Trees.find(function (err, trees) {
+    if (err) 
+        return console.error(err);
+    console.log();
+    console.log("Trees:");
+    console.log(trees);
+})
+
+Types.find(function (err, types) {
+    if (err) 
+        return console.error(err);
+    console.log();
+    console.log("Types:");
+    console.log(types);
+})
+
+Users.find(function (err, users) {
+    if (err) 
+        return console.error(err);
+    console.log();
+    console.log("Users:");
+    console.log(users);
+})
+

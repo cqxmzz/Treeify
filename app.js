6df480128a497c2b9cc1e4cbc6059b97d2dcde0b
lin-li <linli.cs@gmail.com>
2016-04-16 21:29:56 -0700

    add post for plant tree

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static('public'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //=========MODELS=========
 
@@ -21,6 +24,12 @@ app.get('/trees', function(req, res) {
   	res.send(data);
   }
   services.getTrees(models.Trees, models.Types, cob);
+});
+
+app.post('/plant', function(req, res) {
+  console.log(req);
+  var user_id = '5712a546e4b065a8c4d713c6';
+  services.plantTree(models.Trees, models.Users, req, user_id);
 });
 
 app.listen(80, function () {
--- services.js
+++ services.js
@@ -44,3 +44,26 @@ exports.getTrees = function(Trees, Types, cob) {
     });
   });
 }
+
+exports.plantTree = function(Trees, Users, req, user_id) {
+  var tree = {
+  	location: req.body.location,
+  	type: req.body.type,
+  	plant_time: new Date().getTime(),
+  };
+  Trees.create(tree, function(err, tree_saved) {
+    if (err) 
+      return console.error(err);
+
+    Users.findById(user_id, function(err, user) {
+      if (err)
+      	return console.error(err);
+
+      user.trees.push(tree_saved._id);
+      user.save(function(err) {
+      	if (err) 
+      	  return console.error(err);
+      });
+    }); 
+  });
+}

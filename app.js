62808a0d1966de5ec104fd464bd79b58ede4448a
lin-li <linli.cs@gmail.com>
2016-04-16 17:40:29 -0700

    add tree service

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/trees', function(req, res) {
+  var cob = function (data) {
+  	res.send(data);
+  }
+  services.getTrees(models.Trees, models.Types, cob);
+});
+
 app.listen(80, function () {
   console.log('Treeify app listening on port 80!');
 });

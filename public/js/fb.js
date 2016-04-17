e920fa766fe61805610bec7387d050b1294b9723
Qiming Chen <qimche@fb.com>
2016-04-16 21:06:44 -0700

    revert fb.js change

--- public/js/fb.js
+++ public/js/fb.js
@@ -25,17 +25,9 @@ function statusChangeCallback(response) {
 // Button.  See the onlogin handler attached to it in the sample
 // code below.
 function checkLoginState() {
-  FB.getLoginStatus(
-    function(response) {
-      $.get(
-        "/login",
-        response,
-        function(data) {
-           console.log(data);
-        }
-      );
-    }
-  );
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
 }
 
 window.fbAsyncInit = function() {
